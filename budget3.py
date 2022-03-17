abf039f9fc959b3719809d184135a4a51e56ce77
letiviada <99149684+letiviada@users.noreply.github.com>
2022-03-17 12:18:30 +0000

    Add files via upload

--- budget3.py
+++ budget3.py
@@ -35,17 +35,11 @@ class Home(App):
         bt5.pack(fill="both", expand=True)
         bt6.pack(fill="both", expand=True)
 
-class Clothing(Home):
-    def __init__(self,*args,**kwargs):
-        Home.__init__(self,*args,**kwargs)
-        label=tk.Label(self,tex="hello")
-        label.pack(expand=True)
-
 class Addexp(App):
    def __init__(self, *args, **kwargs):
        App.__init__(self, *args, **kwargs)
-    
-    #Button 'Add'
+
+       #Button 'Add'
        def btaddexp():
            dt=date.get()
            u=username.get()
@@ -96,6 +90,32 @@ class Addexp(App):
                    lbl.after(1800,lbl.destroy)
                    lbl.grid(row=2,column=3)
 
+        # Button for Adding income
+       def btaddinc():
+            u=username.get()
+            ni=round(abs(float(inc_ent1.get())),2)
+            inc_lbl6.configure(text="")
+            connection=create_connection("mydatabase.db")
+            get_prev_inc=f"SELECT username,expense,income from login WHERE username='{u}'"
+            ent=read_query(connection,get_prev_inc)
+            prev_exp=round(float(ent[0][1]),2)
+            prev_inc=round(float(ent[0][2]),2)
+            if ent[0][0]==u:
+                    prev_inc=round(prev_inc+ni,2)
+                    balance=prev_inc-prev_exp
+                    add_values=f"UPDATE login SET income='{prev_inc}' WHERE username='{u}'"
+                    inc_ent1.delete(0,tk.END)
+                    execute_query(connection,add_values)
+                    lbl4=tk.Label(self, text=prev_inc,fg="green")
+                    lbl4.grid(row=7, column=1)
+                    lbl5=tk.Label(self,text=balance)
+                    lbl5.grid(row=8,column=1)
+                    if balance>=0:
+                        lbl5.configure(fg="green")
+                    else:
+                        lbl5.configure(fg="red")
+
+
        #Table 1
        for i in range(11):
            if i<6:
@@ -131,7 +151,7 @@ class Addexp(App):
        inc_ent1.grid(row=6,column=1,sticky="ew")
 
       #Buttons
-       btn_addexp=tk.Button(self, text="Add", borderwidth=0)
+       btn_addexp=tk.Button(self, text="Add", borderwidth=0,command=btaddexp)
        btn_addexp.grid(row=4,column=2)
 
        btn_addinc=tk.Button(self, text="Add",borderwidth=0)
@@ -140,7 +160,6 @@ class Addexp(App):
        btn1=tk.Button(self, text="?", borderwidth=0)
        btn1.grid(row=2,column=2)
 
- 
 
 class Account(App):
    def __init__(self, *args, **kwargs):
@@ -148,12 +167,46 @@ class Account(App):
        label = tk.Label(self, text="T")
        label.pack(side="top", fill="both", expand=True)
 
+class View_exp(App):
+    def __init__(self,*args,**kwargs):
+        App.__init__(self,*args,**kwargs)
+        def exp():
+            u=username.get()
+            for i in range(4):
+                label=tk.Label(self,text=names[i],width=10)
+                label.grid(row=1,column=i,sticky="ew")
+
+            get_values=f"SELECT date,amount,category,description from '{u}'"
+            connection=create_connection("mydatabase.db")
+            ent=read_query(connection,get_values)
+            for i in range(len(ent)):
+                dt=ent[i][0]
+                amount=ent[i][1]
+                description=ent[i][2]
+                category=ent[i][3]
+                dt_lbl=tk.Label(self,text=dt)
+                amount_lbl=tk.Label(self,text=amount)
+                category_lbl=tk.Label(self,text=category)
+                description_lbl=tk.Label(self,text=description)
+                dt_lbl.grid(row=i+2,column=0)
+                amount_lbl.grid(row=i+2,column=1)
+                category_lbl.grid(row=i+2,column=2)
+                description_lbl.grid(row=i+2,column=3)
+
+        username_label=tk.Label(self,text="Username: ")
+        username=tk.Entry(self)
+        btn=tk.Button(self,text="See expenses",command=exp)
+        username_label.grid(row=0,column=0)
+        username.grid(row=0,column=1)
+        btn.grid(row=0,column=2)
+
 class Homepageview(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         home = Home(self)
         addexp = Addexp(self)
         account = Account(self)
+        view_exp = View_exp(self)
 
         buttonframe = tk.Frame(self)
         container = tk.Frame(self)
@@ -164,15 +217,17 @@ class Homepageview(tk.Frame):
         home.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         addexp.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         account.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-
+        view_exp.place(in_=container,x=0, y=0, relwidth=1, relheight=1)
 
         b1 = tk.Button(buttonframe, text="Home", command=home.show)
         b2 = tk.Button(buttonframe, text="Add your expenses", command=addexp.show)
         b3 = tk.Button(buttonframe, text="My Account", command=account.show)
+        b4 = tk.Button(buttonframe,text="See your expenses", command=view_exp.show)
 
         b1.pack(side="left")
         b2.pack(side="left")
         b3.pack(side="left")
+        b4.pack(side="left")
 
         home.show()
 
